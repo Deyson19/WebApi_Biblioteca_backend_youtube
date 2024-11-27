f0f0bbb08ab7be32c4b86836d20ea4d9be2118c2
Deyson Venté <deiandresvent@hotmail.com>
2024-11-26 23:10:14 -0500

    corrección ortografía

--- Helpers/CantidadMaximaPrestamo.cs
+++ Helpers/CantidadMaximaPrestamo.cs
@@ -1,33 +1,33 @@
 ﻿namespace WebApp_SistemaBiblioteca.Helpers
 {
-    public static class CantidadMaximaPrestamo
-    {
-        public static DateTime FechaMaximaPrestamo(int idTipoUsuario)
-        {
-            DateTime fechaMaxima = DateTime.Now;
+     public static class CantidadMaximaPrestamo
+     {
+          public static DateTime FechaMaximaPrestamo(int idTipoUsuario)
+          {
+               DateTime fechaMaxima = DateTime.Now;
 
-            switch (idTipoUsuario)
-            {
-                case 1://Afiliado
-                    fechaMaxima = fechaMaxima.AddDays(10);
-                    break;
-                case 2://Empleado o Estudiante
-                    fechaMaxima = fechaMaxima.AddDays(8);
-                    break;
-                case 3://Inivtado
-                    fechaMaxima = fechaMaxima.AddDays(7);
-                    break;
-            }
+               switch (idTipoUsuario)
+               {
+                    case 1://Afiliado
+                         fechaMaxima = fechaMaxima.AddDays(10);
+                         break;
+                    case 2://Empleado o Estudiante
+                         fechaMaxima = fechaMaxima.AddDays(8);
+                         break;
+                    case 3://Invitado
+                         fechaMaxima = fechaMaxima.AddDays(7);
+                         break;
+               }
 
-            if (fechaMaxima.DayOfWeek == DayOfWeek.Saturday)
-            {
-                fechaMaxima = fechaMaxima.AddDays(2);//Lunes
-            }
-            else if (fechaMaxima.DayOfWeek == DayOfWeek.Sunday)
-            {
-                fechaMaxima = fechaMaxima.AddDays(1);//Lunes
-            }
-            return fechaMaxima;
-        }
-    }
+               if (fechaMaxima.DayOfWeek == DayOfWeek.Saturday)
+               {
+                    fechaMaxima = fechaMaxima.AddDays(2);//Lunes
+               }
+               else if (fechaMaxima.DayOfWeek == DayOfWeek.Sunday)
+               {
+                    fechaMaxima = fechaMaxima.AddDays(1);//Lunes
+               }
+               return fechaMaxima;
+          }
+     }
 }
