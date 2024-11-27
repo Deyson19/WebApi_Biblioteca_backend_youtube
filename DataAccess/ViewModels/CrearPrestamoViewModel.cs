55f2f60ba24085d1665f301d5d1a24a5e97fea89
Deyson Venté <deiandresvent@hotmail.com>
2024-11-26 21:30:34 -0500

    Id para actualizarPrestamoViewModel

--- DataAccess/ViewModels/CrearPrestamoViewModel.cs
+++ DataAccess/ViewModels/CrearPrestamoViewModel.cs
@@ -2,20 +2,21 @@
 
 namespace WebApp_SistemaBiblioteca.DataAccess.ViewModels
 {
-    public class CrearPrestamoViewModel
-    {
-        [MaxLength(6,ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
-        public string Isbn { get; set; }
-        [MaxLength(10, ErrorMessage = "El campo {0} no puede superar los {1} caracteres")]
-        public string IdentificacionUsuario { get; set; }
-        public int TipoUsuarioId { get; set; }
-    }
-    public class ActualizarPrestamoViewModel
-    {
-        [MaxLength(10,ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
-        public string Isbn { get; set; }
-        [MaxLength(10, ErrorMessage = "El campo {0} no puede superar los {1} caracteres")]
-        public string IdentificacionUsuario { get; set; }
-        public int TipoUsuarioId { get; set; }
-    }
+     public class CrearPrestamoViewModel
+     {
+          [MaxLength(6, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
+          public string Isbn { get; set; }
+          [MaxLength(10, ErrorMessage = "El campo {0} no puede superar los {1} caracteres")]
+          public string IdentificacionUsuario { get; set; }
+          public int TipoUsuarioId { get; set; }
+     }
+     public class ActualizarPrestamoViewModel
+     {
+          public int Id { get; set; } = 0;
+          [MaxLength(10, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
+          public string Isbn { get; set; }
+          [MaxLength(10, ErrorMessage = "El campo {0} no puede superar los {1} caracteres")]
+          public string IdentificacionUsuario { get; set; }
+          public int TipoUsuarioId { get; set; }
+     }
 }
